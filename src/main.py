02f260bfd3508a90663468dc69ee52fc7a548d16
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-09-02 11:07:48 +0100

    Added multiple ping response and updated ping test

--- src/main.py
+++ src/main.py
@@ -3,22 +3,38 @@
 import socket
 
 
+def handle_client(client_socket):
+    try:
+        while True:
+            # Read the incoming data
+            data = client_socket.recv(1024)
+            if not data:
+                break  # No more data, client closed the connection
+
+            # Check for PING commands in the data and respond accordingly
+            while data:
+                if b"PING" in data:
+                    response = "+PONG\r\n"
+                    client_socket.sendall(response.encode('utf-8'))
+
+                    # Remove the handled command from the data
+                    data = data[data.find(b"PING") + 4:]
+                else:
+                    break
+    finally:
+        # Clean up the connection
+        client_socket.close()
+
+
 def main():
     # Create a TCP/IP socket
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    print("Redis server is listening on port 6379")
+    print("Redis server is listening on port 6379...")
 
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Connection from {client_address}")
-
-        try:
-            # Regardless of input, respond with "+PONG\r\n"
-            response = "+PONG\r\n"
-            client_socket.sendall(response.encode('utf-8'))
-        finally:
-            # Clean up the connection
-            client_socket.close()
+        handle_client(client_socket)
 
 
 if __name__ == "__main__":
--- tests/test_ping.py
+++ tests/test_ping.py
@@ -13,15 +13,20 @@ def start_server():
     # Start the server in a separate thread
     server_thread = threading.Thread(target=main, daemon=True)
     server_thread.start()
-    time.sleep(1)
+    time.sleep(1)  # Give the server a second to start up
 
 
-def test_ping(start_server):
-    """Test that the server responds to PING with +PONG."""
+def test_multiple_pings(start_server):
+    """Test that the server responds to multiple PING commands with multiple +PONG."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(("localhost", 6379))
-        command = "*1\r\n$4\r\nPING\r\n"
-        s.sendall(command.encode('utf-8'))
-        response = s.recv(1024)
-        assert response.decode('utf-8') == "+PONG\r\n"
         
+        # Send two PING commands
+        command = "*1\r\n$4\r\nPING\r\n*1\r\n$4\r\nPING\r\n"
+        s.sendall(command.encode('utf-8'))
+        
+        # Receive the responses
+        response = s.recv(1024)
+        
+        # The response should be "+PONG\r\n+PONG\r\n"
+        assert response.decode('utf-8') == "+PONG\r\n+PONG\r\n"
