2b548908ca7ec00c9e227c7f39aa1f58007e5928
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-09-02 10:46:53 +0100

    Added ping response

---
+++ src/config.py
@@ -0,0 +1,4 @@
+# src/config.py
+
+REDIS_SERVER_HOST = "localhost"
+REDIS_SERVER_PORT = 6379
--- src/main.py
+++ src/main.py
@@ -2,10 +2,25 @@
 
 import socket
 
+from config import *
+
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    # Create a TCP/IP socket
+    server_socket = socket.create_server((REDIS_SERVER_HOST, REDIS_SERVER_PORT), reuse_port=True)
+    print(f"Redis server is listening on port {REDIS_SERVER_PORT}")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address}")
+
+        try:
+            # Regardless of input, respond with "+PONG\r\n"
+            response = "+PONG\r\n"
+            client_socket.sendall(response.encode('utf-8'))
+        finally:
+            # Clean up the connection
+            client_socket.close()
 
 
 if __name__ == "__main__":
